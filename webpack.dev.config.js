af9ca9ff517aedbf7e3a1bf9d7f2e3944417f674
Teemu Pääkkönen <oamaok@gmail.com>
2017-06-14 18:58:41 +0300

    boom

---
+++ webpack.dev.config.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: [
+      './client/sass/main.sass',
+    ],
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'build/'),
+    filename: '[name].[hash].js',
+    publicPath: '/',
+  },
+
+  resolve: {
+    extensions: ['.js', '.sass', '.scss'],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+      inject: 'body',
+      filename: 'index.html',
+    }),
+
+    new webpack.NamedModulesPlugin(),
+  ],
+
+  devtool: 'eval-source-map',
+
+  devServer: {
+    hot: true,
+    contentBase: path.resolve(__dirname, 'build/'),
+    publicPath: '/',
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.s[ac]ss$/,
+        loaders: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|ttf|eot|woff2?|)$/,
+        loader: 'url-loader',
+      },
+    ],
+  },
+};
---
+++ webpack.prod.config.js
@@ -0,0 +1,51 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: [
+      './client/sass/main.sass',
+    ],
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'build/'),
+    filename: '[hash].js',
+    publicPath: '/',
+  },
+
+  resolve: {
+    extensions: ['.js', '.sass', '.scss'],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+      inject: 'body',
+      filename: 'index.html',
+    }),
+
+    new webpack.DefinePlugin({
+      PRODUCTION: true,
+    }),
+
+    new ExtractTextPlugin('[contenthash].css'),
+  ],
+
+  devtool: '',
+
+  module: {
+    loaders: [
+      {
+        test: /\.s[ac]ss$/,
+        loader: ExtractTextPlugin.extract('css-loader!postcss-loader!sass-loader'),
+      },
+      {
+        test: /\.(png|jpe?g|gif|ttf|eot|woff2?|)$/,
+        loader: 'url-loader',
+      },
+    ],
+  },
+};
